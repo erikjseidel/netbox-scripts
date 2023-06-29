d1ff916dbe1be2c5187a5eaa65a337be19f28e31
Erik Seidel <erik@znsl.us>
2023-06-29 09:39:03 +0000

    docfix

--- util/cancel_script.py
+++ util/cancel_script.py
@@ -26,8 +26,8 @@ def cancellable(func):
         try:
             return func(self, *args, **kwargs)
 
-        # Custom abort script handler. Add yaml formatted output rather rather
-        # than no output (which is default action)
+        # Custom abort script handler. Add output rather rather than no output
+        # (which is default action)
         except CancelScript as e:
             self.log_failure(f"Script cancelled with error: {e}")
             self.log_info("Database changes have been reverted due to error.")
